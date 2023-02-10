b72069d6c61b3070e062ec9b6ac60e7f34ef6535
FranciscoJunior12 <105241871+FranciscoJunior12@users.noreply.github.com>
2023-02-11 01:26:57 +0200

    colocando acção no botão salvar do cadastro
    Neste commit dei vida ao botão salvar, configurei as messages e salvei os dados do usuario na tabela de user_auth do django.

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,40 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
+
+
 
 # Create your views here.
 
 def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        
 
-    return render(request, 'cadastro.html')
+        if len(nome.strip())==0 or len(email.strip())==0 or len(senha.strip())==0 or len(confirmar_senha.strip())==0 :
+            messages.add_message(request, constants.ERROR, 'Por favor preencha todos os campos.')
+            return render(request ,'cadastro.html')
+        elif senha != confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'Digite senhas correspondentes.')
+            return render(request ,'cadastro.html')
+        try:
+            user = User.objects.create_user(
+                username= nome,
+                email= email,
+                password=senha
+            )
+            #mensagem de sucesso
+            messages.add_message(request, constants.SUCCESS, 'Usuário criado com sucesso.')
+            return render(request , "cadastro.html")
+        except:
+            #mensagem de erro
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema, Tente novamente dentro de uns minutos.')
+            return render(request , "cadastro.html")
+           
