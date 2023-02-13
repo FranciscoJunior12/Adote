c7d596c3c2227378a36d8674505278db3d0448b5
FranciscoJunior12 <105241871+FranciscoJunior12@users.noreply.github.com>
2023-02-14 01:16:41 +0200

    Criação da view listar e função remover

--- divulgar/views.py
+++ divulgar/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from django.contrib.auth.decorators import login_required
 from .models import Tag, Raca, Pet
 from django.contrib.messages import constants
 from django.contrib import messages
+
 # Create your views here.
 
 
@@ -26,7 +27,7 @@ def novo_pet(request):
 
         # ToDo : validar os dados
         # ToDo : usar API para estado e cidade
-        
+
         pet = Pet(
             usuario=request.user,
             foto=foto,
@@ -39,13 +40,35 @@ def novo_pet(request):
         )
 
         pet.save()
-        
+
         for tag_id in tags:
             tag = Tag.objects.get(id=tag_id)
             pet.tags.add(tag)
 
         pet.save()
-        tags = Tag.objects.all()
-        racas = Raca.objects.all()
-        messages.add_message(request, constants.SUCCESS, 'Novo pet cadastrado')
-        return render(request, 'novo_pet.html', {'tags': tags, 'racas': racas})
+        return redirect('/divulgar/seus_pets')
+       # tags = Tag.objects.all()
+       #racas = Raca.objects.all()
+       #messages.add_message(request, constants.SUCCESS, 'Novo pet cadastrado')
+       # return render(request, 'novo_pet.html', {'tags': tags, 'racas': racas}) 
+
+
+@login_required
+def seus_pets(request):
+    if request.method == "GET":
+        pets = Pet.objects.filter(usuario=request.user)
+        return render(request, 'seus_pets.html', {'pets': pets})
+
+
+@login_required
+def remover_pet(request, id):
+    pet = Pet.objects.get(id=id)
+    if not pet.usuario == request.user:
+        messages.add_message(request, constants.ERROR,
+                             'Esse pet não é seu, espertinho hahaha')
+        return redirect("/divulgar/seus_pets")
+
+    pet.delete()
+    messages.add_message(request, constants.SUCCESS,
+                         'Pet removido com sucesso')
+    return redirect("/divulgar/seus_pets")
